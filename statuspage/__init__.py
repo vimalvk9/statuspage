2de97ac18aa1f67f42e1c834bf1e9f97100684a1
vimalvk9 <kumarvimalvk9@gmail.com>
2018-06-15 18:32:18 +0530

    4

--- statuspage/__init__.py
+++ statuspage/__init__.py
@@ -5,8 +5,9 @@ if DEV_ENV=="HEROKU":
     YA_DEVELOPER_TOKEN=os.environ.get("YELLOWANT_DEVELOPER_TOKEN")
 
     base=os.environ.get("HEROKU_APP_NAME")
-
+    print("asdfdsdfvfdsdf")
     website = "https://{}.herokuapp.com/".format(base)
     os.system("yellowant auth --token {} --host https://www.yellowant.com ".format(YA_DEVELOPER_TOKEN))
     os.system('yellowant sync -q --api_url {}yellowant-api/ --website {} --install_page_url {} --privacy_policy_url {}privacy --redirect_uris {}yellowantredirecturl/'.format(website,website,website,website,website))
     os.system('ls -al')
+    print("sxcdvdsxscd")
